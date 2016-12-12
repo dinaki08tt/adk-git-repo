2600f41083eb5bc669376e235d3d96314f5bcf89
Dinesh Krishnan <dinaki08tt@gmail.com>
2016-12-12 17:14:20 +0530

    hibernate initialize on webapp load

---
+++ sampleapp/src/main/java/com/adk/web/DBStartupListener.java
@@ -0,0 +1,40 @@
+package com.adk.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.adk.db.util.SessionFactoryHelper;
+
+/**
+ * Application Lifecycle Listener implementation class DBStartupListener
+ *
+ */
+public class DBStartupListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public DBStartupListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+    	try{
+			SessionFactoryHelper.getSessionFactory();
+			System.out.println("Session Factory initialized");
+    	}catch(Exception e){
+			e.printStackTrace();
+		}
+    }
+	
+}
