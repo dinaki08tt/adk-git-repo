5d90149bda011fe8d5ad8299d332c76412e18d02
Dinesh Krishnan <dinaki08tt@gmail.com>
2016-12-22 06:08:28 +0530

    Spring secruity 4.1 added fixed the bug

--- sampleapp/src/main/java/com/adk/rest/pingpong/PlayerDetailsService.java
+++ sampleapp/src/main/java/com/adk/rest/pingpong/PlayerDetailsService.java
@@ -1,7 +1,5 @@
 package com.adk.rest.pingpong;
 
-import java.util.Calendar;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,8 +7,11 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.stereotype.Component;
+
 import com.adk.db.pingpong.PlayerDetails;
 import com.adk.db.pingpong.PlayerDetailsHome;
+
 
 @Path("/player")
 public class PlayerDetailsService {
@@ -18,7 +19,6 @@ public class PlayerDetailsService {
 	@GET
 	@Path("/get/{id}")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-//	@Produces({"application/json"})
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public PlayerDetails getPlayerById(@PathParam("id") String id){
 		PlayerDetailsHome dao = PlayerDetailsHome.getInstance();
